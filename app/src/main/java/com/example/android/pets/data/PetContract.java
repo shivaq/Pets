b4035da4d42641e50e40be1121db4ee614d0ca2e
Yasuaki Shibata <shivaq777@gmail.com>
2016-09-26 18:21:50 +0900

    fix:change SQL constants form private to public

--- app/src/main/java/com/example/android/pets/data/PetContract.java
+++ app/src/main/java/com/example/android/pets/data/PetContract.java
@@ -12,7 +12,7 @@ public final class PetContract {
     private static final String INTEGER_TYPE = " INTEGER";
     private static final String COMMA_SEP = ",";
 
-    private static final String SQL_CREATE_ENTRIES =
+    public static final String SQL_CREATE_ENTRIES =
             "CREATE TABLE " + PetEntry.TABLE_NAME + " (" +
                     PetEntry._ID + " INTEGER PRIMARY KEY," +
                     PetEntry.COLUMN_PET_NAME + TEXT_TYPE + COMMA_SEP +
@@ -21,7 +21,7 @@ public final class PetContract {
                     PetEntry.COLUMN_PET_WEIGHT + INTEGER_TYPE +
             " )";
 
-    private static final String SQL_DELETE_ENTRIES =
+    public static final String SQL_DELETE_ENTRIES =
             "DROP TABLE IF EXISTS " + PetEntry.TABLE_NAME;
 
     public static final class PetEntry implements BaseColumns {
