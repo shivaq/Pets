e9a3ce9639dd8e9f75782dc2490eafb18fb80942
Yasuaki Shibata <shivaq777@gmail.com>
2016-09-26 16:23:41 +0900

    Create inner class named Pets for table

--- app/src/main/java/com/example/android/pets/data/PetContract.java
+++ app/src/main/java/com/example/android/pets/data/PetContract.java
@@ -1,8 +1,15 @@
 package com.example.android.pets.data;
 
+import android.provider.BaseColumns;
+
 /**
  * Created by Yasuaki on 2016/09/26.
  */
 
-public class PetContract {
+public final class PetContract {
+
+    public static final class Pets implements BaseColumns {
+
+    }
+
 }
