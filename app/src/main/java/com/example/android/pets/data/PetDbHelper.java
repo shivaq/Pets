592b8a8e9538271475d38fec5e28be8f46802bec
Yasuaki Shibata <shivaq777@gmail.com>
2016-09-26 18:41:46 +0900

    docs:Add some comments

--- app/src/main/java/com/example/android/pets/data/PetDbHelper.java
+++ app/src/main/java/com/example/android/pets/data/PetDbHelper.java
@@ -12,15 +12,26 @@ import android.database.sqlite.SQLiteOpenHelper;
 /*Database helper for Pets app. Manages database creation and version management*/
 public class PetDbHelper extends SQLiteOpenHelper{
 
+    //If you change DB schema, you must increment the DB version
     public static final int DATABASE_VERSION = 1;
     public static final String DATABASE_NAME = "pets.db";
 
+    /**
+     * Constructs a new instance of PetDbHelper
+     *
+     * @param context of the app
+     */
     public PetDbHelper (Context context){
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
+    /**
+     * This is called when the database is created for the first time
+     */
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+
         sqLiteDatabase.execSQL(PetContract.SQL_CREATE_ENTRIES);
     }
 
