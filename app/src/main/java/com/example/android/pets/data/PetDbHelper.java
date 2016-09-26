3052f54c0ea5976606eb54971cec9dfb4b774dff
Yasuaki Shibata <shivaq777@gmail.com>
2016-09-26 22:06:02 +0900

    refactor:rename onUpgrade/onCreate parameter name

--- app/src/main/java/com/example/android/pets/data/PetDbHelper.java
+++ app/src/main/java/com/example/android/pets/data/PetDbHelper.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+//constant は inner class にいるので、PetEntry まで import する
 import com.example.android.pets.data.PetContract.PetEntry;
 
 /*Database helper for Pets app. Manages database creation and version management*/
@@ -11,7 +12,7 @@ public class PetDbHelper extends SQLiteOpenHelper {
 
     //If you change DB schema, you must increment the DB version
     private static final int DATABASE_VERSION = 1;
-    private static final String DATABASE_NAME = "pets.db";
+    private static final String DATABASE_NAME = "shelter.db";
 
     /**
      * Constructs a new instance of PetDbHelper
@@ -19,6 +20,7 @@ public class PetDbHelper extends SQLiteOpenHelper {
      * @param context of the app
      */
     public PetDbHelper(Context context) {
+        //スーパークラスを継承しているから、Context が必要になる？
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
@@ -26,30 +28,32 @@ public class PetDbHelper extends SQLiteOpenHelper {
      * This is called when the database is created for the first time
      */
     @Override
-    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+    public void onCreate(SQLiteDatabase db) {//レクチャーによると、デフォが長いからdb にリネームしてるって。
         //Create String for SQL statement Data types
         final String TEXT_TYPE = " TEXT";
         final String INTEGER_TYPE = " INTEGER";
         final String COMMA_SEP = ",";
 
         //Create a String for SQL statement to create pets table
+        //onCreate される時に何を create したい？ table だろ？
         final String SQL_CREATE_ENTRIES =
                 "CREATE TABLE " + PetEntry.TABLE_NAME + " (" +
-                        PetEntry._ID + " INTEGER PRIMARY KEY," +
+                        PetEntry._ID + INTEGER_TYPE +" PRIMARY KEY," +
                         PetEntry.COLUMN_PET_NAME + TEXT_TYPE + COMMA_SEP +
                         PetEntry.COLUMN_PET_BREED + TEXT_TYPE + COMMA_SEP +
                         PetEntry.COLUMN_PET_GENDER + INTEGER_TYPE + COMMA_SEP +
                         PetEntry.COLUMN_PET_WEIGHT + INTEGER_TYPE +
                         " )";
 
-        sqLiteDatabase.execSQL(SQL_CREATE_ENTRIES);
+        //execSQL は static ではないので、
+        db.execSQL(SQL_CREATE_ENTRIES);
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) {
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
         final String SQL_DELETE_ENTRIES =
                 "DROP TABLE IF EXISTS " + PetEntry.TABLE_NAME;
-        sqLiteDatabase.execSQL(SQL_DELETE_ENTRIES);
+        db.execSQL(SQL_DELETE_ENTRIES);
     }
 }
