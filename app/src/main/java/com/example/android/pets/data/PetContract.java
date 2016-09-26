d9c4ee103c82ae6e725ee6c8e01fc98bc7f16f2f
Yasuaki Shibata <shivaq777@gmail.com>
2016-09-26 16:35:49 +0900

    fix:add constant for _id

--- app/src/main/java/com/example/android/pets/data/PetContract.java
+++ app/src/main/java/com/example/android/pets/data/PetContract.java
@@ -12,6 +12,7 @@ public final class PetContract {
 
         public static final String TABLE_NAME = "pets";
 
+        public static final String COLUMN_ID ="_id";
         public static final String COLUMN_PETS_NAME ="name";
         public static final String COLUMN_BREED = "breed";
         public static final String COLUMN_GENDER = "gender";
