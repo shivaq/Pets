cbccdacdabc567b1b17743593943303444a8a763
Yasuaki Shibata <shivaq777@gmail.com>
2016-09-26 18:02:37 +0900

    feat:Create constatants for DB feat:Create constructor

--- app/src/main/java/com/example/android/pets/data/PetDbHelper.java
+++ app/src/main/java/com/example/android/pets/data/PetDbHelper.java
@@ -1,8 +1,29 @@
 package com.example.android.pets.data;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
 /**
  * Created by Yasuaki on 2016/09/26.
  */
 
-public class PetDbHelper {
+public class PetDbHelper extends SQLiteOpenHelper{
+
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "pets.db";
+
+    public PetDbHelper (Context context){
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
 }
