7c914481e804717c293d11342401a4fbdfd84871
Yasuaki Shibata <shivaq777@gmail.com>
2016-09-29 19:27:11 +0900

    feat:Imprement insertPet() to register user input to database

--- app/src/main/java/com/example/android/pets/EditorActivity.java
+++ app/src/main/java/com/example/android/pets/EditorActivity.java
@@ -15,6 +15,8 @@
  */
 package com.example.android.pets;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
@@ -26,9 +28,11 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.example.android.pets.data.PetContract;
 import com.example.android.pets.data.PetContract.PetEntry;
+import com.example.android.pets.data.PetDbHelper;
 
 /**
  * Allows user to create a new pet or edit an existing one.
@@ -53,6 +57,9 @@ public class EditorActivity extends AppCompatActivity {
      */
     private int mGender = 0;
 
+    //DbHelper that will provide us access to the DB
+    private PetDbHelper mDbHelper;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -65,6 +72,9 @@ public class EditorActivity extends AppCompatActivity {
         mGenderSpinner = (Spinner) findViewById(R.id.spinner_gender);
 
         setupSpinner();
+
+        //instantiate sublcass of SQLiteOpenHelper and pass context of current activity
+        mDbHelper = new PetDbHelper(this);
     }
 
     /**
@@ -107,6 +117,32 @@ public class EditorActivity extends AppCompatActivity {
         });
     }
 
+    /**
+     * Get user input from edittext and save new pet into database
+     */
+    private void insertPet(){
+
+        //get the data repository in write mode
+        SQLiteDatabase db = mDbHelper.getWritableDatabase();
+
+        //Convert editText input to each columns values
+        String name = mNameEditText.getText().toString();
+        String breed = mNameEditText.getText().toString();
+        int weight = Integer.parseInt(mWeightEditText.getText().toString());
+
+        //Create a new map of values, where column names are the keys
+        ContentValues values = new ContentValues();
+        values.put(PetEntry.COLUMN_PET_NAME, name);
+        values.put(PetEntry.COLUMN_PET_BREED, breed);
+        values.put(PetEntry.COLUMN_PET_GENDER, mGender);
+        values.put(PetEntry.COLUMN_PET_WEIGHT, weight);
+
+        long newRowId = db.insert(PetEntry.TABLE_NAME, null, values);
+
+        Toast.makeText(this, name + " is registerd!" + breed + " " +
+                mGender + " " + weight + "kg", Toast.LENGTH_SHORT).show();
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu options from the res/menu/menu_editor.xml file.
@@ -121,7 +157,9 @@ public class EditorActivity extends AppCompatActivity {
         switch (item.getItemId()) {
             // Respond to a click on the "Save" menu option
             case R.id.action_save:
-                // Do nothing for now
+
+                insertPet();
+
                 return true;
             // Respond to a click on the "Delete" menu option
             case R.id.action_delete:
