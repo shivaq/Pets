75138b9852f135599f6fe6a0f762c2b422ce633e
Yasuaki Shibata <shivaq777@gmail.com>
2016-10-01 00:46:28 +0900

    feat: Add UriMatcher to Contract

--- app/src/main/java/com/example/android/pets/data/PetProvider.java
+++ app/src/main/java/com/example/android/pets/data/PetProvider.java
@@ -3,6 +3,7 @@ package com.example.android.pets.data;
 import android.content.ContentProvider;
 import android.content.ContentValues;
 import android.content.Context;
+import android.content.UriMatcher;
 import android.database.Cursor;
 import android.net.Uri;
 
@@ -16,7 +17,42 @@ public class PetProvider extends ContentProvider {
     //declare here and make this global
     private PetDbHelper mDbHelper;
 
-    /** Tag for the log messages */
+    /**
+     * URI matcher code for the content URI for the pets table
+     */
+    private static final int PETS = 100;
+
+    /**
+     * URI matcher code for the content URI for a single pet in the pets table
+     */
+    private static final int PET_ID = 101;
+
+    /**
+     * UriMatcher object to match a content URI to a corresponding code.
+     * The input passed into the constructor
+     * represents the code
+     * to return for the root URI.
+     * It's common to use NO_MATCH as the input for this case.
+     *
+     * コンストラクタ に渡される引数は、URI のルートに戻る事を意味する？？
+     * とにかく、コンストラクタには NO_MATCH を渡すのが通常らしい。
+     * sUriMatcher の s は static の s
+     */
+    private static final UriMatcher sUriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+
+    // Static initializer. This is run the first time anything is called from this class.
+    //Static initializer は、このクラスから何がコールされようが、最初に走るcode
+    static {
+
+        //Add all content URI patterns to UriMatcher.
+        // Provider should recognize them.
+        sUriMatcher.addURI("com.example.android.pets", "pets", PETS);
+        sUriMatcher.addURI("com.example.android.pets", "pets/#", PET_ID);
+    }
+
+    /**
+     * Tag for the log messages
+     */
     public static final String LOG_TAG = PetProvider.class.getSimpleName();
 
     /**
