9fa1b6ceef6fb820f7b6a0782b16a830dd4ac2fc
Yasuaki Shibata <shivaq777@gmail.com>
2016-10-03 01:51:10 +0900

    refactor: Separate getColumnIndex and getString

--- app/src/main/java/com/example/android/pets/PetCursorAdapter.java
+++ app/src/main/java/com/example/android/pets/PetCursorAdapter.java
@@ -8,6 +8,8 @@ import android.view.ViewGroup;
 import android.widget.CursorAdapter;
 import android.widget.TextView;
 
+import com.example.android.pets.data.PetContract.PetEntry;
+
 /**
  * {@link PetCursorAdapter} is an adapter for a list or grid view
  * that uses a {@link Cursor} of pet data as its data source.
@@ -52,14 +54,20 @@ public class PetCursorAdapter extends CursorAdapter {
     @Override
     public void bindView(View view, Context context, Cursor cursor) {
         //Find fields to populate in inflated template
-        TextView tvName = (TextView) view.findViewById(R.id.name);
-        TextView tvBreed = (TextView) view.findViewById(R.id.summary);
+        TextView nameTextView = (TextView) view.findViewById(R.id.name);
+        TextView breedTextView = (TextView) view.findViewById(R.id.summary);
+
+        //Find the columns of pet attributes that we're interested in
+        int nameColumnIndex = cursor.getColumnIndex(PetEntry.COLUMN_PET_NAME);
+        int breedColumnIndex = cursor.getColumnIndex(PetEntry.COLUMN_PET_BREED);
+
         //Extract properties from cursor
-        String name = cursor.getString(cursor.getColumnIndexOrThrow("name"));
-        String breed = cursor.getString(cursor.getColumnIndexOrThrow("breed"));
+        String petName = cursor.getString(nameColumnIndex);
+        String petBreed = cursor.getString(breedColumnIndex);
+
         //Populate fields with extracted properties
-        tvName.setText(name);
-        tvBreed.setText(breed);
+        nameTextView.setText(petName);
+        breedTextView.setText(petBreed);
 
     }
 }
