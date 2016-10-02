f273b247143f80d81d15b2c359b3bfb54a5f442a
Yasuaki Shibata <shivaq777@gmail.com>
2016-10-02 23:44:00 +0900

    feat: Implement newView(), bindView()

--- app/src/main/java/com/example/android/pets/PetCursorAdapter.java
+++ app/src/main/java/com/example/android/pets/PetCursorAdapter.java
@@ -2,9 +2,11 @@ package com.example.android.pets;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CursorAdapter;
+import android.widget.TextView;
 
 /**
  * {@link PetCursorAdapter} is an adapter for a list or grid view
@@ -34,9 +36,7 @@ public class PetCursorAdapter extends CursorAdapter {
      */
     @Override
     public View newView(Context context, Cursor cursor, ViewGroup parent) {
-        // TODO: Fill out this method and return the list item view (instead of null)
-
-        return null;
+        return LayoutInflater.from(context).inflate(R.layout.list_item, parent, false);
     }
 
     /**
@@ -51,6 +51,15 @@ public class PetCursorAdapter extends CursorAdapter {
      */
     @Override
     public void bindView(View view, Context context, Cursor cursor) {
-        // TODO: Fill out this method
+        //Find fields to populate in inflated template
+        TextView tvName = (TextView) view.findViewById(R.id.name);
+        TextView tvBreed = (TextView) view.findViewById(R.id.summary);
+        //Extract properties from cursor
+        String name = cursor.getString(cursor.getColumnIndexOrThrow("name"));
+        String breed = cursor.getString(cursor.getColumnIndexOrThrow("breed"));
+        //Populate fields with extracted properties
+        tvName.setText(name);
+        tvBreed.setText(breed);
+
     }
 }
